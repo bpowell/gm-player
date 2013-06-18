009a788aff9669cfa00302ccdf324f96f34f9221
Brandon Powell <bjpowel2@oakland.edu>
2013-06-18 08:50:53 -0400

    System tray icon can now quit app.

--- gm-player.py
+++ gm-player.py
@@ -33,12 +33,23 @@ def setup():
 	config = Property()
 	config.load(open(configdir+'/'+configfile))
 
+def right_click_systray(icon, button, time):
+	menu = gtk.Menu()
+	quit = gtk.MenuItem("Quit")
+	quit.connect("activate", gtk.main_quit)
+	menu.append(quit)
+	menu.show_all()
+	global systray
+	menu.popup(None, None, gtk.status_icon_position_menu, button, time, systray)
+
 def main():
 	global config
+	global systray
 	setup()
 
 	systray = gtk.StatusIcon()
 	systray.set_from_stock(gtk.STOCK_MEDIA_NEXT)
+	systray.connect("popup-menu", right_click_systray)
 
 	gm_player = browser.Browser(config)
 	DBusGMainLoop(set_as_default=True)
