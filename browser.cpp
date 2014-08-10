9809308a3509937539d0f780273327272b355ce9
Brandon Powell <bpowell29a@gmail.com>
2014-08-09 20:39:41 -0400

    Now using the cookie jar.

--- browser.cpp
+++ browser.cpp
@@ -1,10 +1,16 @@
 #include "browser.h"
+#include "cookiejar.h"
 
 Browser::Browser() {
 	//Flash support
 	QWebSettings::globalSettings()->setAttribute(QWebSettings::PluginsEnabled, true);
 
 	view = new QWebView(this);
+	QNetworkAccessManager *nam = new QNetworkAccessManager();
+	CookieJar *cookies = new CookieJar();
+
+	nam->setCookieJar(cookies);
+	view->page()->setNetworkAccessManager(nam);
 	view->load(QUrl("https://music.google.com"));
 	view->setGeometry(0,0,800,600);
 }
